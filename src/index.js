122f38eba97f7d9ab6151a3af9dda8aa82ccb811
Fadeev Ivan <githubfadeev@gmail.com>
2019-12-13 15:34:00 +0300

    Creating js component files: header.js, table.js, formula.js, toolbar.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './sass/index.scss';
+import {Excel} from './assets/components/excel/excel';
+import {Header} from './assets/components/header/Header';
+import {Toolbar} from './assets/components/toolbar/Toolbar';
+import {Formula} from './assets/components/formula/Formula';
+import {Table} from './assets/components/table/Table';
 
-console.log('Working!');
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table],
+});
+
+excel.render();
