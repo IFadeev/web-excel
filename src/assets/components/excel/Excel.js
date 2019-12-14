5bb704eb93ca19f0eb7968fb9dffe3ac2a488e7b
Fadeev Ivan <githubfadeev@gmail.com>
2019-12-14 15:34:00 +0300

    Created class and methods for working with dom structure

--- src/assets/components/excel/Excel.js
+++ src/assets/components/excel/Excel.js
@@ -1,11 +1,28 @@
+import {$} from '../../../core/dom';
+
 export class Excel {
   constructor(selector, options) {
     this.$el = document.querySelector(selector);
     this.components = options.components || [];
   }
 
+  getRoot() {
+    const $root = $.create('div', 'excel');
+    // const $root = document.createElement('div');
+    // $root.classList.add('excel');
+        
+    this.components.forEach(Component => {
+      const $div = $.create('div', Component.className);
+      // const $div = document.createElement('div');
+      // $div.classList.add(Component.className);
+      const component = new Component($div);
+      $div.innerHTML = component.toHTML();
+      $root.append( $div);
+    });
+    return $root;
+  }
+
   render() {
-    console.log(this.$el);
-    this.$el.insertAdjacentHTML('afterbegin', `<h1>Test<h1>`);
+    this.$el.append(this.getRoot());
   }
 }
